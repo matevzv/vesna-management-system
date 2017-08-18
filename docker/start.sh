cf8620577bdcbde7fe777e823315f254e26d64ac
Matevz Vucnik <vucnikm@gmail.com>
2017-08-18 12:19:35 +0200

    fix docker

--- docker/start.sh
+++ docker/start.sh
@@ -13,6 +13,6 @@ if [ -z "$SERIAL" ]; then
   echo "Serial device missing!"
 else
   cd /root/vesna-management-system
-  sed -i s,10000,"$SERIAL",g webapp/vms/vms.html
+  sed -i s:10000:"$SERIAL":g webapp/vms/vms.html
   java -jar build/VesnaManagementSystem.jar -l false -p "$SERIAL"
 fi
